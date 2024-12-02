d82d0a258fff761f112c217695e4d5cb7c6983be
sharkmerry7 <sharkmerry@gmail.com>
2024-12-01 20:00:28 -0800

    initial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import CookieFlavorSlotMachine from './CookieFlavorSlotMachine';
+
+function App() {
+  return (
+    <div className="App">
+      <CookieFlavorSlotMachine /> {/* Add the component here */}
+    </div>
+  );
+}
+
+export default App

52a090ea672df7a9aad3635640c584e9d6d78dd9
sharkmerry7 <sharkmerry@gmail.com>
2024-12-09 22:49:24 -0800

    chnage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 //import BakeryApp from './cookie-bakery-app'
-import CookieFlavorSlotMachine from './slot.tsx'
+import CookieFlavorSlotMachine from './slot.jsx'
 import './App.css'
 
 function App() {

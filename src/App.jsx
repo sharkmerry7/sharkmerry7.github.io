28db7355e18c50c1b3b9400b4c651da0be170908
sharkmerry7 <sharkmerry@gmail.com>
2024-12-09 22:37:22 -0800

    chnage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import BakeryApp from './cookie-bakery-app'
+//import BakeryApp from './cookie-bakery-app'
+import CookieFlavorSlotMachine from './slot'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      <BakeryApp />
+      <CookieFlavorSlotMachine />
     </div>
   )
 }
